175eddc1ce05fc5fb5bd660b8f49d6bd5c53ac43
Mitch Woodard <woodardcommamitch@gmail.com>
2017-04-16 11:09:35 -0500

    Finished

--- snake.js
+++ snake.js
@@ -1,74 +1,62 @@
 $(document).ready(function(){
-	var gameLoop;
-	var keepPlaying = true;
-	var currentFood;
-	var score = 0;
+	window.gameLoop;	
+	window.currentFood;
+	window.score;
+	window.movedThisTick;	
+	window.keepPlaying;
+	window.snake;
 	
-	snake = new Snake();
-	render();
-	currentFood = makeFood();
-	$(currentFood).addClass('food');
-	gameLoop =	setInterval(function(){ 
-		var headID = "#" + (snake.currentPosition[0][0]+1)+"x"+(snake.currentPosition[0][1]+1);
-		redraw();
+	newGame();
+	startGameLoop();
 
-		if(currentFood == headID){
-			//increment score
-			score += 1;
-			var scorestring = 'Score: ' + score;
-			$('#score').text(scorestring);
-
-			//make new food
-			$(currentFood).removeClass('food');
-			currentFood = makeFood();
-			$(currentFood).addClass('food');
-			
-			//add body segment
-			snake.currentPosition.push([snake.currentPosition[0][0],snake.currentPosition[0][0]]);
-		}
-		if(move()==false){			
-			clearInterval(gameLoop);
-		}						
 	
-	}, 100);
 
 	$(document).keydown(function(event){ 
+		if(movedThisTick == false){
 		switch(event.which){
 			//left
 			case 37:
 				if (snake.direction != "right"){
 					snake.direction = "left";
+					movedThisTick = true;
 				}
 				break;
 			//up
 			case 38:
 				if (snake.direction != "down"){
 					snake.direction = "up";
+					movedThisTick = true;
 				}
 				break;
 			//right
 			case 39:
 				if (snake.direction != "left"){
 					snake.direction = "right";
+					movedThisTick = true;
 				}
 				break;
 			//down
 			case 40:
 				if (snake.direction != "up"){
 					snake.direction = "down";
+					movedThisTick = true;
 				}
 				break;
 		}
+	}
     });								
-	
+	$('#container').on('click','#reset',function(){
+		newGame();
+		startGameLoop();
+		console.log("Game Reset");
+	});
 });
 
 var grid = [];
 
 function Snake() {
-	this.initialPosition=  [20,20],
 	this.direction=  "right",
-	this.currentPosition= [[20,20]];//,[20,19],[20,18],[20,17],[20,16]];
+	this.currentPosition= [[20,20],[20,19],[20,18],[20,17]];
 	
 }
 
@@ -91,14 +79,20 @@ for(i=0;i<40;i++){
 }
 
 function render(){
+	$('#titleContainer').html('');
+	$('#container').html('<div id = "gameWindow" class="grid" tabindex = "1"><div>');
+
 	$('#titleContainer').append($('<h1 id="header">Snake!</h1>'));
 		for(i=0;i<grid.length;i++){
 			for(j=0;j<40;j++){
 				var htmlstring = '<div id = "'+ grid[i][j].id+ '" class = "'+grid[i][j].cssclass+'"> </div>';
 				$(grid[i][j].container).append($(htmlstring));		
 			}			
-		}		
-		$('#container').append($('<h3 id="score">Score: 0</h3>'));
+		}
+		$('#container').append($('<div id="container2"></div>'));		
+		var htmlstring2 = '<h3 id="score">Score: 0</h3>';
+		$('#container2').append($(htmlstring2));
+		$('#container2').append($('<h3 id="reset">Reset</h3>'));
 };
 
 function move(){
@@ -122,6 +116,12 @@ function move(){
 			//snake.currentPosition[0][0] += 1;
 			tempSnake[0] += 1;	
 			break;			
+	}
+	var i = snake.currentPosition.length;
+	while(i--){
+		if (tempSnake[0] == snake.currentPosition[i][0] && tempSnake[1] == snake.currentPosition[i][1] ){
+			return false;
+		}
 	}			
 
 	snake.currentPosition.unshift(tempSnake);
@@ -147,7 +147,44 @@ function makeFood(){
 	var randomRow = Math.floor(Math.random() * 40)+1;
 	var randomColumn = Math.floor(Math.random() * 40)+1;
 	var foodCell = "#" + randomRow+"x"+randomColumn;
-	//console.log("#" + randomRow+"x"+randomColumn);	
-	//foodCell.addClass('food');	
 	return foodCell;
 }
+
+function newGame(){
+	render();
+	score = 0;
+	movedThisTick = false;	
+	keepPlaying = true;
+	snake = new Snake();
+	currentFood = makeFood();
+	$(currentFood).addClass('food');
+}
+
+function startGameLoop(){
+	gameLoop =	setInterval(function(){ 
+		movedThisTick = false;
+		var headID = "#" + (snake.currentPosition[0][0]+1)+"x"+(snake.currentPosition[0][1]+1);
+		redraw();
+
+		//if food gets eaten
+		if(currentFood == headID){
+			//increment score
+			score += 1;
+			var scorestring = 'Score: ' + score;
+			$('#score').text(scorestring);
+
+			//make new food
+			$(currentFood).removeClass('food');
+			currentFood = makeFood();
+			$(currentFood).addClass('food');
+			
+			//add body segment
+			snake.currentPosition.push([snake.currentPosition[0][0],snake.currentPosition[0][0]]);
+		}
+		//if there are no movement options, get out of the loop
+		if(move()==false){			
+			clearInterval(gameLoop);
+		}			
+	}, 100);
+}
+
